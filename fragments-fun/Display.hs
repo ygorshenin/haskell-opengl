a74bd007b28b4937aec4752370c31d9cafd4776f
Yuri Gorshenin <mipt.vi002@gmail.com>
2015-09-12 23:01:31 +0300

    Code improvements.

--- fragments-fun/Display.hs
+++ fragments-fun/Display.hs
@@ -11,9 +11,11 @@ import Y.Buffers
 import Y.Context
 import Y.Shaders
 
+-- Vertices mixed with colors. There are 3 groups, first 2 floats in
+-- each group are coords, other numbers are colors.
 vertices = [ 0.5, (-0.5),    1.0, 0.0, 0.0
            , (-0.5), (-0.5), 0.0, 1.0, 0.0
-           , 0.0, 0.5,        0.0, 0.0, 1.0
+           , 0.0, 0.5,       0.0, 0.0, 1.0
            ] :: [GL.GLfloat]
 
 display :: IORef Context -> IO ()
@@ -25,13 +27,14 @@ display ctxRef = do
   GL.bindBuffer GL.ArrayBuffer $= Just vbo
   withArray vertices $ \ptr -> do
       GL.bufferData GL.ArrayBuffer $= (listSize vertices, ptr, GL.StaticDraw)
+
+  -- Initialize vertex coords (20 bytes stride since there're 5 floats in each group).
   GL.vertexAttribPointer (GL.AttribLocation 0) $= (GL.ToFloat, GL.VertexArrayDescriptor 2 GL.Float 20 (bufferOffset 0))
   GL.vertexAttribArray (GL.AttribLocation 0) $= GL.Enabled
 
+  -- Initialize vertex colors (20 bytes stride since there're 5 floats in each group).
   GL.vertexAttribPointer (GL.AttribLocation 1) $= (GL.ToFloat, GL.VertexArrayDescriptor 3 GL.Float 20 (bufferOffset 8))
   GL.vertexAttribArray (GL.AttribLocation 1) $= GL.Enabled
-
-  GL.bindVertexArrayObject $= Nothing
 
   let shaders = [ ShaderInfo "shaders/triangle.vert" GL.VertexShader
                 , ShaderInfo "shaders/triangle.frag" GL.FragmentShader
@@ -39,13 +42,18 @@ display ctxRef = do
   program <- mkProgram shaders
   GL.currentProgram $= Just program
 
+  offsetLocation <- GL.uniformLocation program "offset"
+
   let loop = do
       GLFW.pollEvents
       ctx <- readIORef ctxRef
       unless (ctxQuit ctx) $ do
           GL.clearColor $= GL.Color4 0.2 0.3 0.3 1
           GL.clear [GL.ColorBuffer]
-          GL.bindVertexArrayObject $= Just vao
+
+          time <- GL.get GLFW.time
+          GL.uniform offsetLocation $= GL.Vertex2 (0.5 * (realToFrac $ sin time)) (0.0 :: GL.GLfloat)
+
           GL.drawArrays GL.Triangles 0 3
           GLFW.swapBuffers
           loop
--- fragments-fun/shaders/triangle.vert
+++ fragments-fun/shaders/triangle.vert
@@ -3,10 +3,12 @@
 layout (location = 0) in vec2 position;
 layout (location = 1) in vec3 color;
 
+uniform vec2 offset;
+
 out vec3 outColor;
 
 void main()
 {
-  gl_Position = vec4(position, 0.0, 1.0);
+  gl_Position = vec4(offset + position, 0.0, 1.0);
   outColor = color;
 }
